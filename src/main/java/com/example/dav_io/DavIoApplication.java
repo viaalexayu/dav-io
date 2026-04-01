b052bfc6d2eea73efb343f8bf551774ac142e2ac
ViaYuSecE <via_alexa_yu@yahoo.com>
2026-04-01 18:49:58 -0400

    Initial commit

---
+++ src/main/java/com/example/dav_io/DavIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dav_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dav_io/DavIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dav_io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
